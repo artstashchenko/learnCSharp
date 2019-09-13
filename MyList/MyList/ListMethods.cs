34abb3764d9f68dce031e3605d94e06ab09880e7
Slavik <sst@stylepit.com>
2019-09-13 16:05:46 +0300

    master
    first Iteration

---
+++ MyList/MyList/ListMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    public class ListMethods
+    {
+        private int _index = 0;
+        private int _sizeArray = 1;
+        private int[] _items = new int[1];
+
+        public void Add(int value)
+        {
+            if (_items[_index] != 0)
+            {
+                _index++;
+                _sizeArray++;
+                Array.Resize(ref _items, _sizeArray);
+            }
+            _items[_index] = value;
+        }
+
+        public int Get(int index)
+        {
+            return 0;
+        }
+    }
+}
---
+++ MyList/MyList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            ListMethods myList = new ListMethods();
+
+            myList.Add(1);
+            myList.Add(0);
+            myList.Add(2);
+            myList.Add(3);
+
+            Console.WriteLine("Item 0 is 1: {0}", myList.Get(0) == 1);
+            Console.WriteLine("Item 0 is 0: {0}", myList.Get(1) == 0);
+            Console.WriteLine("Item 0 is 2: {0}", myList.Get(2) == 2);
+            Console.WriteLine("Item 0 is 3: {0}", myList.Get(3) == 3);
+        }
+    }
+}
