0655259fbef9f98a02eccde3680b324917ffc8e1
Slavik <sst@stylepit.com>
2019-09-23 18:38:39 +0300

    master - six Iteration

--- MyList/MyList/Program.cs
+++ MyList/MyList/Program.cs
@@ -13,6 +13,7 @@ namespace MyList
             myList.Add(2);
             myList.Add(7);
             myList.Add(3);
+            myList.Add(7);
 
             myList.RemoveAt(3);
 
@@ -20,7 +21,8 @@ namespace MyList
             Console.WriteLine("Item 0 is 0: {0}", myList.Get(1) == 0);
             Console.WriteLine("Item 0 is 2: {0}", myList.Get(2) == 2);
             Console.WriteLine("Item 0 is 3: {0}", myList.Get(3) == 3);
-            Console.WriteLine("Item Count is 4: {0}", myList.Count() == 4);
+            Console.WriteLine("Item 0 is 7: {0}", myList.Get(3) == 7);
+            Console.WriteLine("Item Count is 5: {0}", myList.Count() == 5);
         }
     }
 }
