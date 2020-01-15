17712ddf8796dafcd04cdbb40aeff5bf3620c62e
Slavik <sst@stylepit.com>
2020-01-15 17:22:38 +0200

    2,3 days of learning

--- C#/SimpleProgramm/SimpleProgramm/Program.cs
+++ C#/SimpleProgramm/SimpleProgramm/Program.cs
@@ -2,17 +2,93 @@
 
 namespace SimpleProgramm
 {
-    class Program
-    {
-        static void Main(string[] args)
+    class CastExpr {
+        static void Main()
         {
-            double radius; //test variable
-            double area; //test variable
+            double n;
 
-            radius = 10.0;
-            area = radius * radius * 3.1416;
-
-            Console.WriteLine("Area is equal: " + area);
+            for (n = 1.0; n <= 10; n++)
+            {
+                Console.WriteLine("Квадратный корень из {0} равен {1}", n, Math.Sqrt(n));
+                Console.WriteLine("d: {0}", (int)Math.Sqrt(n));
+                Console.WriteLine("Дробная часть числа: {0}", Math.Sqrt(n) - (int)Math.Sqrt(n));
+            }
         }
     }
+    //class ScopeDemo {
+
+    //    static void Main() {
+    //        int x;
+    //        x = 10;
+    //        if (x == 10)
+    //        {
+    //            int y = 20;
+    //            x = y * 2;
+    //        }
+    //        Console.WriteLine("x equals " + x);
+    //    }
+    //}
+    //class DisplayOptions {
+    //    static void Main()
+    //    {
+    //        int i;
+    //        Console.WriteLine("Number/Square/Cube");
+    //        for (i = 1; i < 10; i++)
+    //            Console.WriteLine("{0}\t{1}\t{2}", i, i*i, i*i*i);
+    //    }
+    //}
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        double radius; //test variable
+    //        double area; //test variable
+
+    //        radius = 10.0;
+    //        area = radius * radius * 3.1416;
+
+    //        Console.WriteLine("Area is equal: " + area);
+    //        Earth();
+    //    }
+
+    //    static void Earth()
+    //    {
+    //        long inches;
+    //        long miles;
+
+    //        miles = 93000000;
+    //        inches = miles * 5280 * 12;
+    //        Console.WriteLine("Distance to sun: " + inches);
+    //    }
+    //}
+
+    //class Use_byte {
+    //    static void Main() 
+    //    {
+    //        byte x;
+    //        int sum;
+    //        sum = 0;
+    //        for (x = 1; x <= 100; x++)
+    //            sum = sum + x;
+
+    //        Console.WriteLine("Sum from 1 to 100 is equal: " + sum);
+    //   }
+    //}
+
+    //class NestVar
+    //{
+    //    static void Main()
+    //    {
+    //        int count;
+
+    //        for (count = 0; count < 10; count = count + 1)
+    //        {
+    //            Console.WriteLine("Something: " + count);
+
+    //            int count;
+    //            for (count = 0; count < 2; count++)
+    //                Console.WriteLine("Issue appears here");
+    //        }
+    //    }
+    //}
 }
